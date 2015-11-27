51631031569102b93aeda5fbdabaed0bbed16fb1
francois <francois.caron59@gmail.com>
2015-11-27 21:35:41 +0100

    finish

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@ module.exports = {
 		'clientSecret' : process.env.SESSION_SECRET || 'secretmeanproject',
 	},
 	'facebookAuth' : {
-		'clientID'     : process.env.FB_ID,
-		'clientSecret' : process.env.FB_SECRET,
+		'clientID'     : process.env.FB_ID || '1729934680559589',
+		'clientSecret' : process.env.FB_SECRET || 'wrong_secret',
 		'callbackURL'  : '/api/public/auth/facebook/callback'
 	}
 };
