34dc38e9670cdc01c499886821fb3c52f6271aa1
francois <francois.caron59@gmail.com>
2015-12-22 23:24:18 +0100

    typescript

---
+++ test/test.js
@@ -0,0 +1,12 @@
+"use strict";
+var test;
+var test1;
+var test21;
+test = "  ";
+test1 = "";
+test21 = '';
+function a(ar) {
+    this.ar = ar;
+}
+;
+console.log(a('a'));
---
+++ test/test.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+let test: String;
+let test1: String;
+let test21: String;
+
+test = "  ";
+test1 = "";
+test21 = '';
+
+function a(ar: String): void {
+	this.ar = ar;
+};
+
+console.log(a('a'));
